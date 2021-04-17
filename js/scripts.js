dd31db2d6616ceb04d142250ed9ff1f0ed2bd6df
Charles Mwathi <mwathi.kinyua@gmail.com>
2021-04-17 16:05:42 +0300

    Add custom and jQuery scripts

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+  const sum = function(number01, number02) {
+    return number01 + number02;
+  }
+
+  
+  $("form#triangleForm").submit(function(event) {
+    event.preventDefault();
+    const side01 = parseInt($("input#side01").val());
+    const side02 = parseInt($("input#side02").val());
+    const side03 = parseInt($("input#side03").val());
+  
+    const sum01 = sum(side01, side02);
+    const sum02 = sum(side01, side03);
+    const sum03 = sum(side02, side03);
+  
+    if (side01 === side02 && side01 === side03 && side02 === side03) {
+      $("#equilateralTriangle").show();
+      $("#isoscelesTriangle").hide();
+      $("#scaleneTriangle").hide();
+      $("#notTriangle").hide();
+    } else if(side01 === side02 && side01 !== side03 || side01 === side03 && side01 !== side02 || side02 === side03 && side02 !== side01) {
+      $("#isoscelesTriangle").show();
+      $("#equilateralTriangle").hide();
+      $("#scaleneTriangle").hide();
+      $("#notTriangle").hide();
+    } else if(side01 !== side02 && side01 !== side03 && side02 !== side03) {
+      $("#scaleneTriangle").show();
+      $("#equilateralTriangle").hide();
+      $("#isoscelesTriangle").hide();
+      $("#notTriangle").hide();
+    } else if(sum01 <= side03 || sum02 <= side03 || sum03 <= side03){
+      $("#notTriangle").show();
+      $("#equilateralTriangle").hide();
+      $("#isoscelesTriangle").hide();
+      $("#scaleneTriangle").hide();
+    }
+
+    $("#triangleOutput").show();
+  });
+});
